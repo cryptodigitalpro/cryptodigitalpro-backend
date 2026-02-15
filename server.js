ae087b457c8ee223575dd4268f2d2d49872264d1
CryptoDigitalPro <nalukulu604@gmail.com>
2026-02-15 06:45:07 -0800

    Fixed CORS to allow production frontend

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "https://cryptodigitalpro.com",
+    "https://www.cryptodigitalpro.com"
+  ],
   credentials: true
 }));
 
